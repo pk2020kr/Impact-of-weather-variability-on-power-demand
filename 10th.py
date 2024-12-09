9f3ca245059b98234372e011bcbc9523d63063e7
prabhat <pk2020kr@email.com>
2024-12-09 11:59:29 +0530

    v6 of 10.py

--- 10th.py
+++ 10th.py
@@ -57,3 +57,14 @@ print (4>=2)  #greater then and equal to
 #print (4=<2) 
 #print (4=>2)
 
+x = -10
+
+if x > 0:
+    print('positive')
+elif x == 0:
+    print('zero')
+else:
+    print('negative')
+    
+    
+    
