3e5fcdb7ae12129aaf6e931f6160587c7ff2e52d
prabhat <pk2020kr@email.com>
2024-12-09 12:29:40 +0530

    v2 of 8th.py

--- 8th.py
+++ 8th.py
@@ -10,3 +10,21 @@ print(type(-5.5))
 print(type("What's the type for this data"))
 #Boolean
 print(type(True))
+
+
+
+# Check if given number is prime
+# print "PRIME" or "NOT PRIME" 
+n = 32
+is_prime = True
+
+for number in range(2,n):
+    if n%number==0:
+        print("NOT PRIME")
+        is_prime = False
+        break
+
+if is_prime==True:
+    print("PRIME")
+    
+    
