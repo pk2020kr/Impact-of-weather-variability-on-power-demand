5003349ea79d5933a483fd188f5c465a494b9d98
prabhat <pk2020kr@email.com>
2024-12-11 19:05:41 +0530

    v2* of 12th

--- 12th.py
+++ 12th.py
@@ -17,3 +17,33 @@ print(a)
 
 
 
+def burger_maker():
+    print("Need 2 buns")
+    print("Add cheese")
+    print("Add patty")
+    print("Add Sauces")
+    print("Burger is ready")
+    
+
+def pizza_maker():
+    print("Need pizza base")
+    print("Add cheese")
+    print("Add Mushrooms")
+    print("Add onion")
+    print("Pizza is ready")
+    
+
+# Orders are coming
+
+# Pizza
+pizza_maker()
+print()
+# Burger
+burger_maker()
+print()
+# Pizza
+pizza_maker()
+print()
+# Burger
+burger_maker()
+
